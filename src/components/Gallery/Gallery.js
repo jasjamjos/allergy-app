e659738fef2db1774919ec4228b376c89e43b4a2
jasper <jasjamjos@gmail.com>
2020-06-12 00:37:13 +0800

    update

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -19,9 +19,9 @@ const Gallery = (props) => {
                             </ul>
                         </div>
                         <div className="px-6 py-4">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#cats</span>
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#dogs</span>
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#ducks</span>
                         </div>
                     </div>
                 )
